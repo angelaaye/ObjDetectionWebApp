54cc2fb9c2a8ff85d0e301158dcdc33cb2af6ac4
Feilong Song <feilong.song@mail.utoronto.ca>
2020-02-07 15:53:20 -0500

    Lazy loading

--- frontend/opencv-app/src/router/index.js
+++ frontend/opencv-app/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,40 +7,27 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/ImageUpload.vue')
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/register',
     name: 'register',
-    component: () => import(/* webpackChunkName: "register" */ '../views/Register.vue')
+    component: () => import('../views/Register.vue')
   },
   {
     path: '/upload',
     name: 'upload',
-    component: () => import(/* webpackChunkName: "register" */ '../views/Upload.vue')
+    component: () => import('../views/Upload.vue')
   },
   {
     path: '/result',
     name: 'result',
     component: () => import('../views/Thumbnails.vue')
-  },
-  {
-    path: '/view/:id',
-    component: () => import('../views/View.vue'),
-    props: true
   }
 ]
 
